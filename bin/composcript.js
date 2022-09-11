27aab3f25400963b7ff93a9a33eea253a85d8deb
Nicolas Gouwy <43481095+Iconejey@users.noreply.github.com>
2022-09-11 16:44:12 +0200

    Handle `init` command

--- bin/composcript.js
+++ bin/composcript.js
@@ -1,4 +1,9 @@
 #!/usr/bin/env node
+const fs = require('fs');
+const rl = require('readline').createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
 
 const colors = {
 	red: '\x1b[31m',
@@ -11,12 +16,47 @@ const colors = {
 	reset: '\x1b[0m'
 };
 
+// If package.json is not found, exit
+if (!fs.existsSync('package.json')) {
+	console.log(`${colors.red}package.json not found${colors.reset}`);
+	process.exit(1);
+}
+
+// Get package.json
+const package = JSON.parse(fs.readFileSync('package.json'));
+
 // Get the arguments
 const arg = process.argv[2];
 
 // If user wants to init CompoScript config
 if (arg === 'init') {
 	console.log(`${colors.green}Initializing CompoScript config${colors.reset}`);
+
+	const config = {};
+
+	// Components directory
+	rl.question(`${colors.yellow}Components directory${colors.reset} (default: ./components): `, dir => {
+		config.components = dir || './components';
+
+		// Styles type
+		rl.question(`${colors.yellow}Styles type${colors.reset} (default: scss): `, type => {
+			// Styles directory
+			rl.question(`${colors.yellow}Styles directory${colors.reset} (default: ./styles): `, dir => {
+				config[type || 'scss'] = dir || './styles';
+
+				// Log config
+				console.log('Adding config to package.json:');
+				console.log(config);
+
+				// Save config
+				package.composcript = config;
+				fs.writeFileSync('package.json', JSON.stringify(package, '\n', 4));
+
+				// Exit
+				rl.close();
+			});
+		});
+	});
 }
 
 // If user wants to create a new component
